feb9057edae0f7e33324c24a827efaa0a42172fa
jorge.sousa <jorge.sousa@velocix.com>
2022-03-18 08:04:34 +0000

    backend: add BackendErr enum and Score struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,41 @@
-#[macro_use] extern crate rocket;
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use]
+extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+// in v0.5.1-rc Json is part of rocket
+// https://stackoverflow.com/questions/68682054/how-to-return-json-as-a-response-in-rust-rocket-with-auto-field-deserialising
+// but we are still using 0.4.10 for now
+use rocket_contrib::json::Json;
+use serde::{Deserialize, Serialize};
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Responder, Debug)]
+pub enum BackendErr {
+    #[response(status = 403)]
+    Unauthorized(String),
+    #[response(status = 404)]
+    NotFound(String),
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[derive(Serialize, Clone, Debug)]
+pub struct Score {
+    pub id: i32,
+    pub score: i32,
+    pub player: String,
+}
+
+#[get("/list")]
+fn list() -> Result<Json<Vec<Score>>, BackendErr> {
+    Ok(Json(vec![Score {
+        id: 0,
+        score: 100000,
+        player: "BestPlayer".to_string(),
+    }]))
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/highscores", routes![list])
+        .launch();
 }
