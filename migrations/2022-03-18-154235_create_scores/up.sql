e1349e1048a3e2c69ea02c0176655d7136710a10
sousajf1 <jorge.pinto.sousa@protonmail.ch>
2022-03-22 11:53:10 +0000

    backend: get user data for each level and difficulty

--- migrations/2022-03-18-154235_create_scores/up.sql
+++ migrations/2022-03-18-154235_create_scores/up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE scores (
    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
    high_score INTEGER NOT NULL,
-   username VARCHAR NOT NULL
+   username TEXT NOT NULL,
+   difficulty TEXT CHECK( difficulty IN ('TRAINING', 'EASY', 'MEDIUM', 'HARD', 'ZATOICHI') ) NOT NULL,
+   level TEXT CHECK( level IN ('1', '2', '3') ) NOT NULL
 )
